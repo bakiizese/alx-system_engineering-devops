f714ffa962ca8cf70da29ada30e4c340cd75d8f3
bakiizese <bereketzeselassie@gmail.com>
2024-01-29 05:55:12 -0800

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''api response json'''
+import requests
+import sys
+
+
+rl = 'todos'
+rl2 = 'users'
+url = 'https://jsonplaceholder.typicode.com/'
+
+response = requests.get(url + rl2)
+
+if response.status_code == 200:
+    json_data = response.json()
+    for k in json_data:
+        if k['id'] == int(sys.argv[1]):
+            name = k['name']
+    response2 = requests.get(url + rl)
+    if response2.status_code == 200:
+        json_data = response2.json()
+        all = 0
+        done = 0
+        donelist = []
+        for i in json_data:
+            if i['userId'] == int(sys.argv[1]):
+                all += 1
+                if i['completed'] is True:
+                    donelist.append(i['title'])
+                    done += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, done, all))
+    for i in donelist:
+        print('\t' + i)
+
+
+else:
+    print(f"Error: {response.status_code}")
