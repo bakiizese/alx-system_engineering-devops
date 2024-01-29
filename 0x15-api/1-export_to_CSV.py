40e66a64f16a4a6772e7a1e8ab5a0f72f5dc94a2
bakiizese <bereketzeselassie@gmail.com>
2024-01-29 08:55:04 -0800

    try

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,9 +14,8 @@ if __name__ == '__main__':
 
     task_by_ids = []
     for tasks in todo:
-        if tasks.get('completed') is True:
-            task_by_ids.append(tasks.get('title'))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user.get('name'), len(task_by_ids), len(todo)))
-
-    print("\n".join("\t {}".format(tsk) for tsk in task_by_ids))
+        ts = "{}","{}","{}","{}".format(ids, user.get('name'), tasks.get('conpleted'), tasks.get('title'))
+        task_by_ids.append(ts)
+    
+    for i in task_by_ids:
+        print(i)
