edaac2d89f2842d920151286d4f42e235bbee7a7
bakiizese <bereketzeselassie@gmail.com>
2024-02-13 06:27:11 -0800

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit."""
+import requests
+import sys
 
 
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Chrome/79.0.3945.130 Safari/537.36'}
