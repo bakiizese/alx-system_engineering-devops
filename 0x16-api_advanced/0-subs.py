d5e8ad65e699d64df12f73b44c04f0bfa65ccd07
bakiizese <bereketzeselassie@gmail.com>
2024-02-13 06:07:17 -0800

    fi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,10 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    #user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    head = {'User-Agent': 'My User Agent 1.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    response = get(url, headers=head)
 
     try:
         response.raise_for_status()  # Raise an exception for HTTP errors
