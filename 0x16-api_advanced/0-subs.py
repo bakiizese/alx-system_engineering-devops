4191be3619e5bb725f844ebce21812055d206668
bakiizese <bereketezeselassie@gmial.com>
2024-02-13 16:25:52 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''return the total number of subscribers in a given subreddit'''
-    if (subreddit is None or type(subrddit) is not str):
+    if (subreddit is None or type(subreddit) is not str):
         return 0
     redit = requests.get(url='https://www.reddit.com/r/{}/\
                              about.json'.format(subreddit))
