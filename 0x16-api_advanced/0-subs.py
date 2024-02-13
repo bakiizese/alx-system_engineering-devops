b9ecc31769d86af3259fb2c0bf21c13a0d851fc3
bakiizese <bereketzeselassie@gmail.com>
2024-02-13 05:24:05 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''return the total number of subscribers in a given subreddit'''
+    if (subreddit is None or type(subrddit) is not str):
+        return 0
     redit = requests.get(url='https://www.reddit.com/r/{}/\
                              about.json'.format(subreddit))
     if (redit.status_code == 200):
